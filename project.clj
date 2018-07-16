15b45960cac470d52e059b023aa51e4d8c8c5f21
fmind <dev@fmind.me>
2018-07-16 13:16:18 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euphony "0.1.0"
   :description "A friendly translator in a world full of dangerous malware."
-  :license {:name "AGPLv3" :url "https://opensource.org/licenses/agpl-3.0"}
+  :license {:name "EUPL 1.2" :url "https://github.com/fmind/euphony/blob/master/LICENSE.txt"}
   :url "https://github.com/fmind/euphony"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.4"]
